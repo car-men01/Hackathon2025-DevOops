156d9a56f5ba18f3524ceedc2175ec625d9e7d2e
ANRazvan <alexe.razvan05@gmail.com>
2025-11-22 18:33:01 +0200

    asdasdasd

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     host: '0.0.0.0',
-    port: 3000,
-    strictPort: false,
+    port: parseInt(process.env.PORT || '3000'),
+    strictPort: true,
   },
   server: {
     host: '0.0.0.0',
